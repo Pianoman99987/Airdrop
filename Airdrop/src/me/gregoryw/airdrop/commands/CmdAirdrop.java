63bae2b7e9030f06fae52507ff288708a6978237
geweese <geweese@ncsu.edu>
2018-08-28 17:32:24 -0400

    Updated CmdAirdrop

--- Airdrop/src/me/gregoryw/airdrop/commands/CmdAirdrop.java
+++ Airdrop/src/me/gregoryw/airdrop/commands/CmdAirdrop.java
@@ -1,5 +1,6 @@
 package me.gregoryw.airdrop.commands;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
@@ -7,53 +8,79 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import me.gregoryw.airdrop.Airdrop;
 
 public class CmdAirdrop implements CommandExecutor {
 
+	private static final Airdrop instance = new Airdrop();
+	private boolean noBlocksAbovePlayer;
+
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		
+
 		// Check if sender is a Player
-		if(sender instanceof Player) {
-			
+		if (sender instanceof Player) {
+
 			// Cast sender to Player
 			Player player = (Player) sender;
-			
+
 			// If no arguments, return false
-			if(args.length == 0) {
+			if (args.length == 0) {
 				return false;
 			}
-			
-			//TODO
-			// Airdrop is a kit
-			if(args.length == 1) {
-				
+
+			if (args.length == 3) {
+
 				Location loc = player.getLocation();
-				double newHeight = loc.getY() + 10;
-				
-				Location loc2 = new Location(player.getWorld(), loc.getX(), newHeight, loc.getZ());
-				
-				player.getWorld().spawnFallingBlock(loc2, Material.OAK_PLANKS, (byte) 0);
-				
+
+				noBlocksAbovePlayer = true;
+
+				if (noBlocksAbovePlayer(loc)) {
+					// Do stuff
+					return true;
+				} else {
+					// Send some error
+					System.out.println("No space above player");
+				}
+
 			}
-			
-			// Airdrop item and amount
-			if(args.length == 2) {
-				
-				//Material mat = Material.getMaterial(args[0].split(":")[1]);		
-				
+
+			if (args.length == 4) {
+
 			}
-			
-			// Airdrop is invalid
-			if(args.length > 2) {
-				return false;
-			}
-			
-			
+
 		}
-		
+
 		// If sender isn't player, return false
 		return false;
+	}
+
+	/**
+	 * Checks if there are blocks within 20 blocks above player
+	 * 
+	 * @param loc
+	 * @return
+	 */
+	private boolean noBlocksAbovePlayer(Location loc) {
+
+		Bukkit.getScheduler().runTask(instance, new Runnable() {
+
+			@Override
+			public void run() {
+
+				for (int x = 0; x < 20; x++) {
+
+					if (loc.getBlock().getType() != Material.AIR) {
+						noBlocksAbovePlayer = false;
+					}
+
+					loc.add(0, 1, 0);
+				}
+			}
+
+		});
+
+		return noBlocksAbovePlayer;
 	}
 
 }
