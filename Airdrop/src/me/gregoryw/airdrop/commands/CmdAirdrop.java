5b348e70ce9f5d9f9e563d2d6342da27df9fd058
geweese <geweese@ncsu.edu>
2018-09-02 11:55:43 -0400

    Added more modules to CmdAirdrop

--- Airdrop/src/me/gregoryw/airdrop/commands/CmdAirdrop.java
+++ Airdrop/src/me/gregoryw/airdrop/commands/CmdAirdrop.java
@@ -16,9 +16,6 @@ import me.gregoryw.airdrop.helpers.ChatHandler;
 
 public class CmdAirdrop implements CommandExecutor {
 
-	/** Private Instance Variables */
-	private boolean noBlocksAbovePlayer;
-
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
@@ -35,34 +32,18 @@ public class CmdAirdrop implements CommandExecutor {
 
 			// Send package to sender where only argument is the package
 			if (args.length == 1) {
-				
+
 				String packageName = args[0];
-				
-				ArrayList<ItemStack> items = new ArrayList<ItemStack>();
-				
-				if(PackagesConfig.getConfig().contains("packages." + packageName)) {
-					int itemNumber = 1;
-					while(PackagesConfig.getConfig().contains("packages." + packageName + ".items." + itemNumber)) {
-						items.add(PackagesConfig.getConfig().getItemStack("packages." + packageName + ".items." + itemNumber));
-						itemNumber++;
-					}
-				} else {
-					ChatHandler.sendMessage(player, "This is an invalid package!");
+
+				ArrayList<ItemStack> items = getItemsInPackage(packageName, player);
+
+				if (items.isEmpty()) {
 					return false;
 				}
- 
+
 				Location loc = player.getLocation();
-				
-				noBlocksAbovePlayer = true;
-				
-				for (int x = 0; x < 20; x++) {
 
-					if (!loc.getBlock().getType().equals(Material.AIR)) {
-						noBlocksAbovePlayer = false;
-					}
-
-					loc.add(0, 1.0, 0);
-				}
+				boolean noBlocksAbovePlayer = checkBlocksAbovePlayer(loc);
 
 				if (noBlocksAbovePlayer) {
 
@@ -93,6 +74,49 @@ public class CmdAirdrop implements CommandExecutor {
 
 		// If sender isn't player, return false
 		return false;
+	}
+
+	/**
+	 * Returns an ArrayList of ItemStack of items in the specified package. If the
+	 * package is invalid, returns an empty ArrayList
+	 * 
+	 * @param packageName
+	 * @param player
+	 * @return
+	 */
+	private ArrayList<ItemStack> getItemsInPackage(String packageName, Player player) {
+
+		ArrayList<ItemStack> itemsToReturn = new ArrayList<ItemStack>();
+
+		if (PackagesConfig.getConfig().contains("packages." + packageName)) {
+			int itemNumber = 1;
+			while (PackagesConfig.getConfig().contains("packages." + packageName + ".items." + itemNumber)) {
+				itemsToReturn.add(
+						PackagesConfig.getConfig().getItemStack("packages." + packageName + ".items." + itemNumber));
+				itemNumber++;
+			}
+		} else {
+			ChatHandler.sendMessage(player, "This is an invalid package!");
+		}
+
+		return itemsToReturn;
+
+	}
+	
+	private boolean checkBlocksAbovePlayer(Location loc) {
+		boolean isAbove = true;
+		
+		for (int x = 0; x < 20; x++) {
+
+			if (!loc.getBlock().getType().equals(Material.AIR)) {
+				isAbove = false;
+			}
+
+			loc.add(0, 1.0, 0);
+		}
+		
+		
+		return isAbove;
 	}
 
 }
